06c9249e3fb8599af143d3fb598f34a77c012d1e
Inaki Villar <inaki.villar@gotinder.com>
2020-03-10 16:32:55 -0700

    macing

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 //comment pero se sdddiii
-//commendsdddijt psssercccvcdo se sdddiii
+//commendsdddijteireerureiueroireoerio psssercccvcdo se sdddiii
 //comment pero se sdddiii
     //sssssss
     override fun onCreate(savedInstanceState: Bundle?) {
