e6f1cd089fe08ef3419a4f736c4786a8e3cf387a
Inaki Villar <inaki.villar@gotinder.com>
2020-03-10 16:25:46 -0700

    test4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 //comment pero se sdddiii
 //commendsdddijt psssercccvcdo se sdddiii
 //comment pero se sdddiii
+    //sssssss
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
