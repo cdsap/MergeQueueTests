ccc758092bcda87d868dea33ac37383169a83ed6
Inaki Villar <inaki.villar@gotinder.com>
2020-03-10 14:57:57 -0700

    adding initial import

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//comment pero se sdddiii
+//commendsdddijt pero se sdddiii
+//comment pero se sdddiii
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
